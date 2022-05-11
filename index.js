2fe589b09ee7c8673098516cd66cc3dbbca5211c
assraf-nur <assraf.nur@gmail.com>
2022-05-12 00:40:59 +0600

    create database connect with mongo and show data in ui via service link

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const app = express();
 require('dotenv').config()
@@ -6,6 +7,30 @@ const port = process.env.PORT || 5000
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@doctors-portal.etdd8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const servicesCollection = client.db('docPortal').collection('services')
+
+        app.get('/services', async(req, res) =>{
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
