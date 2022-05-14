76c8f04c5e4c1edd1c15350a249f5cfb05b551f6
assraf-nur <assraf.nur@gmail.com>
2022-05-14 14:22:43 +0600

    added a api for showing available data

--- index.js
+++ index.js
@@ -1,48 +1,92 @@
 // essential links for connection
-const express = require('express');
+const express = require("express");
 // mongo link
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 // for using cors
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
-require('dotenv').config()
-const port = process.env.PORT || 5000
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
 // mongo database connection link
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@doctors-portal.etdd8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 // function for receive and send data in client site
-async function run(){
-    try{
-        await client.connect();
-        const servicesCollection = client.db('docPortal').collection('services')
+async function run() {
+  try {
+    await client.connect();
+    const servicesCollection = client.db("docPortal").collection("services");
+    const bookingCollection = client.db("docPortal").collection("bookings");
 
-        // send data to client site
-        // all data sent
-        app.get('/services', async(req, res) =>{
-            const query = {};
-            const cursor = servicesCollection.find(query);
-            const services = await cursor.toArray();
-            res.send(services);
-        })
-    }
-    finally{
+    // send data to client site
+    // all data sent
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
 
-    }
+    app.get("/available", async (req, res) => {
+      const date = req.query.date || "May 14, 2022";
+
+      // step 1: get all services
+      const services = await servicesCollection.find().toArray();
+
+      // get the booking of that day 
+      const query  = {date: date};
+      const bookings = await bookingCollection.find(query).toArray();
+
+      // for each service, find booking for that service
+      services.forEach(service => {
+        // find the bookings for that service[{}, {}, {}]
+        const serviceBookings = bookings.filter(book => book.treatment === service.name);
+        // take or select slots for the service bookings['','','']
+        const booked = serviceBookings.map(book => book.slot);
+        // select those slots which are not in book slots 
+        const available = service.slots.filter(slot => !booked.includes(slot));
+        // set available to slots to make it easier
+        service.slots = available;
+      })
+
+      res.send(services);
+    });
+
+
+    
+    // get the single appointment data
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+      const query = {
+        treatment: booking.treatment,
+        date: booking.date,
+        patient: booking.patient,
+      };
+      const exists = await bookingCollection.findOne(query);
+      if (exists) {
+        return res.send({ success: false, booking: exists });
+      }
+      const result = await bookingCollection.insertOne(booking);
+      return res.send({ success: true, result });
+    });
+  } finally {
+  }
 }
-run().catch(console.dir)
+run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Hello from Doctor");
+});
 
-app.get('/', (req, res) => {
-    res.send("Hello from Doctor")
-})
-
-app.listen(port, () =>{
-    console.log(`Doctor is running on ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Doctor is running on ${port}`);
+});
