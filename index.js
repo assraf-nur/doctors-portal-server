5ee99ae26718431e7a0e3f742dffea0d7beb5c39
assraf-nur <assraf.nur@gmail.com>
2022-05-15 01:03:03 +0600

    add token

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 // for using cors
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -50,7 +51,8 @@ async function run() {
         updateDoc,
         options
       );
-      res.send(result);
+      const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      res.send({result, token});
     });
 
     app.get("/available", async (req, res) => {
